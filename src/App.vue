aaf9e1bccb90b5e5bce3ecd5f31cb81826bf4000
atayiilmaz <yiilmazataberk@gmail.com>
2025-03-15 00:23:50 +0300

    some minor practices about dynamic datas

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
-<template>
-  <h1>Vue Jobs</h1>
-</template>
-
-<style scoped>
-  h1 {
-    color: red;
+<script>
+  export default {
+    data () {
+      return {
+        name: 'Ata',
+        status: '',
+        tasks: ['task 1', 'task 2', 'task 3'],
+        link: 'https://www.google.com'
+      }
+    }
   }
-</style>
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <h3>Tasks: </h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+
+  <a :href="link">Click for Google</a>
+</template>
