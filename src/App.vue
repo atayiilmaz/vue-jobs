17d45365acee6be66e0f727df09da57b8d91c4d7
atayiilmaz <yiilmazataberk@gmail.com>
2025-03-15 13:13:27 +0300

    feat: add navbar and hero components

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,9 @@
-<script>
-  export default {
-    data () {
-      return {
-        name: 'Ata',
-        status: '',
-        tasks: ['task 1', 'task 2', 'task 3'],
-        link: 'https://www.google.com'
-      }
-    },
-    methods: {
-      toggleStatus () {
-          if (this.status === 'active') {
-            this.status = 'pending';
-          } else if (this.status === 'pending') {
-            this.status = 'inactive';
-          } else {
-            this.status = 'active';
-        }
-      },
-    }
-  }
+<script setup>
+  import Navbar from '@/components/Navbar.vue';
+  import Hero from '@/components/Hero.vue';
 </script>
 
 <template>
-  <h1>{{ name }}</h1>
-  <p v-if="status === 'active'">User is active</p>
-  <p v-else-if="status === 'pending'">User is pending</p>
-  <p v-else>User is inactive</p>
-
-  <h3>Tasks: </h3>
-  <ul>
-    <li v-for="task in tasks" :key="task">{{ task }}</li>
-  </ul>
-
-  <a :href="link">Click for Google</a>
-  <br>
-  <button @click="toggleStatus">Change status</button>
+  <Navbar />
+  <Hero />
 </template>
