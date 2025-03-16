67fa54b17bc40c642ff40d14d8af890dc51ec5f3
atayiilmaz <yiilmazataberk@gmail.com>
2025-03-17 01:18:50 +0300

    feat: add single job page

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import JobsView from "@/views/JobsView.vue";
-import NotFound from "@/views/NotFoundView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
+import JobView from "@/views/JobView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,10 +18,15 @@ const router = createRouter({
             component: JobsView
         },
         {
+            path: "/jobs/:id",
+            name: "job",
+            component: JobView
+        },
+        {
             path: "/:catchAll(.*)",
             name: "not-found",
             component: NotFoundView
-        }
+        },
     ],
 });
 
