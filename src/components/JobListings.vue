ce2cad0af783a8fac0229c30978750b5e0a6c791
atayiilmaz <yiilmazataberk@gmail.com>
2025-03-17 14:17:27 +0300

    feat: implement json-server and vue-spinner

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,8 +1,9 @@
 <script setup>
-    import { ref, defineProps } from 'vue';
+    import { reactive, defineProps, onMounted } from 'vue';
     import JobListing from './JobListing.vue';
-    import jsonData from '@/jobs.json';
     import { RouterLink } from 'vue-router';
+    import axios from 'axios';
+    import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
 
     defineProps({
         limit: Number,
@@ -12,7 +13,21 @@
         }
     });
 
-    const jobs = ref(jsonData);
+    const state = reactive({
+        jobs: [],
+        isLoading: true
+    });
+
+    onMounted(async () => {
+        try {
+            const response = await axios.get('http://localhost:5000/jobs');
+            state.jobs = response.data;
+        } catch (error) {
+            console.error(error);   
+        } finally {
+            state.isLoading = false;
+        }
+    });
 </script>
 
 <template>
@@ -21,9 +36,13 @@
             <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
                 Browse Jobs
             </h2>
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                
-                <JobListing v-for="job in jobs.slice(0, limit) || jobs.length" :key="job.id" :job="job" />
+            <!-- loading spinner while loading is true -->
+            <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+                <PulseLoader />
+            </div>
+
+            <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <JobListing v-for="job in state.jobs.slice(0, limit) || state.jobs.length" :key="job.id" :job="job" />
             </div>
         </div>
     </section>
