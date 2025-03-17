8ba2dd64ab55dbf19e27b5933b661fb3ba5fba32
atayiilmaz <yiilmazataberk@gmail.com>
2025-03-17 17:57:05 +0300

    feat: create single job page and add job form and handling forms

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -2,10 +2,13 @@
     import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
     import BackButton from '@/components/BackButton.vue';
     import { reactive, onMounted } from 'vue';
-    import { useRoute, RouterLink } from 'vue-router';
+    import { useRoute, RouterLink, useRouter } from 'vue-router';
+    import {useToast} from 'vue-toastification';
     import axios from 'axios';
 
     const route = useRoute();
+    const router = useRouter();
+    const toast = useToast();
     const jobId = route.params.id;
 
     const state = reactive({
@@ -23,6 +26,17 @@
             state.loading = false;
         }
     });
+
+    const deleteJob = async () => {
+        try {
+            await axios.delete(`/api/jobs/${jobId}`);
+            toast.success('Job deleted successfully');
+            router.push('/jobs');
+        } catch (error) {
+            console.error(error);
+            toast.error('An error occurred while deleting the job');
+        }
+    }
 </script>
 
 <template>
@@ -40,7 +54,7 @@
                 class="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
               >
                 <i
-                  class="fa-solid fa-location-dot text-lg text-orange-700 mr-2"
+                  class="pi pi-map-marker text-orange-700"
                 ></i>
                 <p class="text-orange-700">{{ state.job.location }}</p>
               </div>
@@ -94,7 +108,7 @@
                 class="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >Edit Job
                 </RouterLink>
-              <button
+              <button @click="deleteJob"
                 class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
               >
                 Delete Job
